3507335a831f1a2e7e4e0b93b3ac08ee63a0e3cd
Carles Capdevila <capdecarles@gmail.com>
2021-03-27 00:18:00 +0100

    Fix custom troops crash, remove debug logging

--- TrainTroops/TrainTroopsBehaviour.cs
+++ TrainTroops/TrainTroopsBehaviour.cs
@@ -2,8 +2,6 @@
 using TaleWorlds.Core;
 using System.Collections.Generic;
 using System.Linq;
-using TaleWorlds.Localization;
-using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
 
 namespace TrainTroops
 {
@@ -26,6 +24,8 @@ namespace TrainTroops
             if (party.IsActive && !party.IsLeaderless && party.IsMainParty && party.LeaderHero != null && party.LeaderHero == Hero.MainHero)
             {
 
+                int leaderLeadership = Hero.MainHero.GetSkillValue(DefaultSkills.Leadership);
+
                 int totalXPEarned = 0;
                 Dictionary<string, int> troopsReadyToUpgrade = new Dictionary<string, int>();
                 for (int i = 0; i < party.MemberRoster.Count; i++)
@@ -35,13 +35,9 @@ namespace TrainTroops
                     //Only gain XP if character LVL is lower than the leader's LVL
                     if (troop.Character.Level < Hero.MainHero.Level)
                     {
-                        int leaderLeadership = Hero.MainHero.GetSkillValue(DefaultSkills.Leadership);
                         int lvlDifference = Hero.MainHero.Level - troop.Character.Level;
                         int trainableTroopCount = troop.Number - troop.NumberReadyToUpgrade;
 
-                        //Gain XP: leadership skill * 3 + level difference * 10
-                        Logger("Troop: " + LocalizedTextManager.GetTranslatedText(LocalizedTextManager.DefaultEnglishLanguageId, troop.Character.Name.GetID()) + " Leadership: " + leaderLeadership + " xpEarnedByLevel: " + leaderLeadership * troopXPMultiplier + " xpByLevelDifference: " + lvlDifference * levelDifferenceMultiplier + " trainable troop count: " + trainableTroopCount);
-                        
                         //Perform the math
                         int xpEarned = (leaderLeadership * troopXPMultiplier + lvlDifference * levelDifferenceMultiplier) * trainableTroopCount;
                         party.Party.MemberRoster.AddXpToTroopAtIndex(xpEarned, i);
@@ -52,10 +48,11 @@ namespace TrainTroops
                             //Will update the party button notification so that the red icon is shown (?)
                             //PlayerUpdateTracker.Current.GetPartyNotificationText();
                             //PlayerUpdateTracker.Current.UpdatePartyNotification();
-                            
-                            //TODO: get the localized troop name, for now it only gets it in english
-                            string troopName = LocalizedTextManager.GetTranslatedText(LocalizedTextManager.DefaultEnglishLanguageId, troop.Character.Name.GetID());
+
+                            //TODO: get the troop name, for now it only gets it in english
+                            string troopName = troop.Character.ToString();
                             //Count how many troops of each type are ready to upgrade
+
 
                             //If a troop with the same name has already been counted, add it
                             if (troopsReadyToUpgrade.ContainsKey(troopName))
@@ -73,7 +70,7 @@ namespace TrainTroops
                     }
                 }
 
-                InformationManager.DisplayMessage(new InformationMessage("Total training XP for the day: " + totalXPEarned + getTroopsReadyToUpgradeMessage(troopsReadyToUpgrade)));
+                InformationManager.DisplayMessage(new InformationMessage("Total training XP for the day: " + totalXPEarned + "." + getTroopsReadyToUpgradeMessage(troopsReadyToUpgrade)));
                 
             }
 
@@ -96,17 +93,6 @@ namespace TrainTroops
                 troopsReadyToUpgradeMessage += ".";
             }
             return troopsReadyToUpgradeMessage;
-        }
-
-        public static void Logger(string lines)
-        {
-            //Write the string to a file.append mode is enabled so that the log
-            //lines get appended to  test.txt than wiping content and writing the log
-
-            using (System.IO.StreamWriter file = new System.IO.StreamWriter("./trainTroops.log", true))
-            {
-                file.WriteLine(lines);
-            }
         }
 
         public override void SyncData(IDataStore dataStore)
